c3d31130b310b1cd4079ea368325c7a7f384b074
Amir Khalifa <amir.kh28@hotmail.com>
2024-06-05 19:51:15 +0300

    Fetch from json & switch

--- HW3/js/scripts.js
+++ HW3/js/scripts.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     initRectangles();
+    document.querySelector('header div:nth-child(2) span').onclick = switchRectanglesSongs;
 }
 
 const colors = ['lightcoral', 'lightgreen', 'lightblue', 'violet'];
@@ -25,6 +26,20 @@ function initRectangles() {
 }
 
 function initSongs() {
+    console.log('Loading songs...');
+    fetch('../data/music.json')
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Network response was not ok' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            populateSongsInList(data);
+        })
+        
+        .catch(err => console.error('Error fetching data...', err));
+       
 }
 
 function chooseRectangleColor() {
@@ -40,7 +55,30 @@ function subtractRectangle() {
 }
 
 function switchRectanglesSongs() {
+    const wrapper = document.getElementById('wrapper');
+    if (wrapper.className === 'rectangles') {
+        initSongs();
+        document.querySelector('header div:nth-child(2) span').textContent = 'Switch to rectangles';
+    } else {
+        initRectangles();
+        document.querySelector('header div:nth-child(2) span').textContent = 'Switch to songs';
+    }
 }
 
-function populateSongsInList() {
+function populateSongsInList(data) {
+    const wrapper = document.getElementById('wrapper');
+    wrapper.innerHTML = '';
+    wrapper.className = 'songs';
+
+    const title = document.createElement('h1');
+    title.textContent = 'Spotify top 2024 songs';
+    wrapper.appendChild(title);
+
+    const list = document.createElement('ol');
+    data.songs.forEach(song => {
+        const listItem = document.createElement('li');
+        listItem.textContent = song;
+        list.appendChild(listItem);
+    });
+    wrapper.appendChild(list);
 }
