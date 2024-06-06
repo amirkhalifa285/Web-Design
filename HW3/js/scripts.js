f9a6d673c1a5b47b944477e495f8948de180dae0
Amir Khalifa <amir.kh28@hotmail.com>
2024-06-06 13:26:33 +0300

    JavaScript Validator error fixes

--- HW3/js/scripts.js
+++ HW3/js/scripts.js
@@ -1,14 +1,15 @@
-window.onload = () => {
-    initRectangles();
-    document.querySelector('header div:nth-child(2) span').onclick = switchRectanglesSongs;
-    document.querySelector('header div:first-child span').onclick = addRectangle;
-    document.querySelector('header div:last-child span').onclick = subtractRectangle;
-}
+/* global document, window, console, fetch */
 
 const colors = ['lightcoral', 'lightgreen', 'lightblue', 'violet'];
 const letters = 'AmirKhalifa'.split('');
 let colorIndex = 0;
 let letterIndex = 0;
+
+function chooseRectangleColor() {
+    const color = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+    return color;
+}
 
 function initRectangles() {
     const wrapper = document.getElementById('wrapper');
@@ -23,31 +24,41 @@ function initRectangles() {
         rect.appendChild(span);
         wrapper.appendChild(rect);
     });
-    colorIndex = 0 ;
+    colorIndex = 0;
     letterIndex = letters.length;
 }
 
+function populateSongsInList(data) {
+    const wrapper = document.getElementById('wrapper');
+    wrapper.innerHTML = '';
+    wrapper.className = 'songs';
+
+    const title = document.createElement('h1');
+    title.textContent = data.title;
+    wrapper.appendChild(title);
+
+    const list = document.createElement('ol');
+    data.songs.forEach(song => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${song.name} - ${song.artist}`;
+        list.appendChild(listItem);
+    });
+    wrapper.appendChild(list);
+}
+
 function initSongs() {
-    console.log('Loading songs...');
-    fetch('../data/music.json')
+    fetch('data/songs.json')
         .then(response => {
-            if(!response.ok) {
-                throw new Error('Network response was not ok' + response.statusText);
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
             }
             return response.json();
         })
         .then(data => {
             populateSongsInList(data);
         })
-        
+        // eslint-disable-next-line no-console
         .catch(err => console.error('Error fetching data...', err));
-       
-}
-
-function chooseRectangleColor() {
-    const color = colors[colorIndex];
-    colorIndex = (colorIndex + 1) % colors.length;
-    return color;
 }
 
 function addRectangle() {
@@ -56,7 +67,7 @@ function addRectangle() {
         const rect = document.createElement('div');
         rect.className = 'rectangle';
         rect.textContent = letters[letterIndex % letters.length];
-        letterIndex++;
+        letterIndex += 1; 
         rect.style.backgroundColor = chooseRectangleColor();
         wrapper.appendChild(rect);
     }
@@ -82,20 +93,9 @@ function switchRectanglesSongs() {
     }
 }
 
-function populateSongsInList(data) {
-    const wrapper = document.getElementById('wrapper');
-    wrapper.innerHTML = '';
-    wrapper.className = 'songs';
-
-    const title = document.createElement('h1');
-    title.textContent = data.title;
-    wrapper.appendChild(title);
-
-    const list = document.createElement('ol');
-    data.songs.forEach(song => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${song.name} - ${song.artist}`;
-        list.appendChild(listItem);
-    });
-    wrapper.appendChild(list);
-}
+window.onload = () => {
+    initRectangles();
+    document.querySelector('header div:nth-child(2) span').onclick = switchRectanglesSongs;
+    document.querySelector('header div:first-child span').onclick = addRectangle;
+    document.querySelector('header div:last-child span').onclick = subtractRectangle;
+};
